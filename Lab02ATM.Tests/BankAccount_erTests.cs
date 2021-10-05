31f2e2d893c9bf50d38656882733629f4ccffdf2
ymclapp <ymclapp@yahoo.com>
2021-10-05 15:30:42 -0500

    scaffolding set up

---
+++ Lab02ATM.Tests/BankAccount_erTests.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Lab02ATM.Tests
+{
+  public class BankAccount_erTests
+  {
+    [Fact]
+    public void ex_FizzBuzz_returns_1_given_1()
+    {
+      //Arrange
+      //FizzBuzzer fb = new FizzBuzzer();
+      //Act
+      //string result = fb.FizzBuzz(1);
+      //Assert
+      //Assert.Equal("1", result);
+    }
+
+    [Fact]
+    public void balance_shows_correct_amount()
+    {
+      //Arrange
+      //BankAccount atm = new BankAccount();
+      //Act
+      //string result = fb.FizzBuzz(1);
+      //Assert
+      //Assert.Equal("1", result);
+    }
+  }
+}
---
+++ LabATM/BankAccount.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabATM
+{
+  public class BankAccount
+  {
+    private static double balance = 0;
+
+
+  }
+
+
+}
