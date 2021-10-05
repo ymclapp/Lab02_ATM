118385bbdd820dfe95e405dcc680b2958ca64055
ymclapp <ymclapp@yahoo.com>
2021-10-05 12:41:19 -0500

    added base files

---
+++ LabATM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
