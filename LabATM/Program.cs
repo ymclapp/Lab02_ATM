b15daf0bcc7df24d842dd64b1cfabd7910484c06
ymclapp <ymclapp@yahoo.com>
2021-10-07 10:19:24 -0500

    fixed the PromptForAction() build issue.  It is working now.

--- LabATM/Program.cs
+++ LabATM/Program.cs
@@ -8,32 +8,32 @@ namespace LabATM
     static void Main(string[] args)
     {
       WriteWelcome();
-      //int PromptForAction();
+      PromptForAction();
 
       static void WriteWelcome()
       {
         Console.WriteLine("Welcome to your friendly ATM!\n");
       }//this is the end of the WriteWelcome()
 
-      static int PromptForAction()
+      static void PromptForAction()
       {
         String promptResponse;
         int userResponse;
         try
         {
-          Console.WriteLine("Shall we play a game?  What would you like to do today?\n");
+          Console.WriteLine("Shall we play a game?  What would you like to do today?");
           Console.WriteLine("Enter 1 if you would like your current balance");
           Console.WriteLine("Enter 2 if you would like to make a deposit");
           Console.WriteLine("Enter 3 if you would like to make a withdrawal");
           Console.WriteLine("Enter 0 if you would like to quit\n");
-          Console.WriteLine("Please enter a number from the selections above:  \n");
+          Console.WriteLine("Please enter a number from the selections above:  ");
           promptResponse = Console.ReadLine();
           userResponse = Convert.ToInt32(promptResponse);
-          Console.WriteLine("You selected option " + userResponse);
+          Console.WriteLine ("You selected option " + userResponse);
         }
         catch
         {
-          Console.WriteLine("There was an error");
+          Console.WriteLine ("There was an error");
         }
       }//this is the end of the PromptForAction()
     }//this is the end of the Main()
