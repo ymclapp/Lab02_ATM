7b1bfd418741a42c946b26fc1c1326dc208c7f4f
ymclapp <ymclapp@yahoo.com>
2021-10-09 16:35:15 -0500

    added some of the system IO, but it is not writing to file

--- LabATM/BankAccount.cs
+++ LabATM/BankAccount.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LabATM
 {
   public class BankAccount
   {
     private decimal balance;  //balance here will default to 0 if no value given
+
+    
+
     public decimal GetBalance()
     {
-      return balance;
+      return this.balance;
     }
 
     public void Deposit(decimal depositAmount)
@@ -19,6 +23,7 @@ namespace LabATM
       if (depositAmount < 0)
         throw new ArgumentOutOfRangeException("Amount must be positive");
       else balance += depositAmount;
+      WriteToLog(depositAmount);
     }
 
     public void Withdrawal(decimal withdrawalAmount)
@@ -28,9 +33,19 @@ namespace LabATM
       else if (withdrawalAmount < 0)
         Console.WriteLine("Please enter a positive amount to withdraw");
        else balance -= withdrawalAmount;
-    }
+      WriteToLog(withdrawalAmount);
+    }//end of withdrawalAmount
 
-  }
+    public static void WriteToLog(decimal amount)
+    {
+      //string fileText = File.ReadAllLines(filename);
+      string logMessage = $"{DateTime.Today: yyyy-MM-dd}: {amount}\n";
+      File.AppendAllText("log.txt", logMessage);
+    }//end of WriteToLog()
 
-
-}
+    public static void ClearLogFile()
+    {
+      File.WriteAllText("log.txt", "");
+    }//end of ClearLogFile()
+  }//end of BankAccount class
+}//end of namespace LabATM
